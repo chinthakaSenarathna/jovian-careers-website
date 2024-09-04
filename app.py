9e1716fe885e2cc8575b5a2aad2b92a2528a247f
chinthaka senarathna <chironprabath@gmail.com>
2024-09-04 13:57:34 +0530

    built first web page using flask

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-import demo
+from flask import Flask
+from jinja2.ext import debug
 
-print("Hello..." + __name__)
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "hello jovian"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
--- demo.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello...")
-    print("Welcome User...")
-
-if __name__ == "__main__":
-    main()
