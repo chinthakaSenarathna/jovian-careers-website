583ed538d8746c90184e6c434d451e2b0cf2fe39
chinthaka senarathna <chironprabath@gmail.com>
2024-09-05 00:09:01 +0530

    Jinja2 Syntax -> Used to insert value of jobs variable into the HTML

--- app.py
+++ app.py
@@ -3,9 +3,43 @@ from jinja2.ext import debug
 
 app = Flask(__name__)
 
+# list of dictionary...
+JOBS = [
+    {
+        'id': 1,
+        'tittle': 'Data Analysis',
+        'location': 'Kandy',
+        'salary': 'Rs. 10000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Science',
+        'location': 'Colombo',
+        'salary': 'Rs. 12000'
+    },
+    {
+        'id': 3,
+        'title': 'Full Stack Developer',
+        'location': 'Colombo',
+        'salary': 'Rs. 8000'
+    },
+    {
+        'id': 4,
+        'title': 'Front-end Developer',
+        'location': 'Gale',
+        'salary': 'Rs. 4000'
+    },
+    {
+        'id': 5,
+        'title': 'Back-end Developer',
+        'location': 'Colombo',
+        'salary': 'Rs. 5000'
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
